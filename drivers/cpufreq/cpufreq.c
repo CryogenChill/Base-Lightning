597dc89682f64c12a46f2744973bc3795ea0f58c
Shoaib0597 <Shoaib0595@gmail.com>
2015-11-16 20:19:07 +0530

    Added support for Darkness and IntelliDemand Governors
    # Conflicts:
    #	drivers/cpufreq/Kconfig
    #	drivers/cpufreq/Makefile
    #	drivers/cpufreq/cpufreq.c
    #	include/linux/cpufreq.h

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1826,6 +1826,7 @@ int __cpufreq_driver_getavg(struct cpufreq_policy *policy, unsigned int cpu)
 }
 EXPORT_SYMBOL_GPL(__cpufreq_driver_getavg);
 
+ 
 static int __cpufreq_governor(struct cpufreq_policy *policy,
 					unsigned int event)
 {
